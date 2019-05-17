f0d47ccbcb1dac1920f50a2b6c6eefd5c33a6a39
Azize <achraf.azize@polytechnique.edu>
2019-05-17 15:48:41 +0200

    SEDataBaseCreatorAppDescriptor.hpp

---
+++ DataBaseCreator/include/SEDataBaseCreatorAppDescriptor.hpp
@@ -0,0 +1,28 @@
+/// \headerfile SBProxy.hpp "SBProxy.hpp"
+#include "SBProxy.hpp"
+
+/// \headerfile SEDataBaseCreatorApp.hpp "SEDataBaseCreatorApp.hpp"
+#include "SEDataBaseCreatorApp.hpp"
+
+
+// Class descriptor
+
+// SAMSON Element generator pro tip: complete this descriptor to expose this class to SAMSON and other SAMSON Elements
+
+SB_CLASS_BEGIN(SEDataBaseCreatorApp);
+
+	SB_CLASS_TYPE(SBCClass::App);
+        SB_CLASS_DESCRIPTION("SEDataBaseCreatorApp : SAMSON Element generator pro tip: modify me");
+
+	SB_FACTORY_BEGIN;
+
+		SB_CONSTRUCTOR_0(SEDataBaseCreatorApp);
+
+	SB_FACTORY_END;
+
+	SB_INTERFACE_BEGIN;
+
+	SB_INTERFACE_END;
+
+SB_CLASS_END(SEDataBaseCreatorApp);
+
